d01488ba9222187bfb3e965f0bfd96be36bcc99c
Donray Williams <w.donray@yahoo.com>
2017-02-05 12:22:03 -0600

    A bit of logic change with combat

--- CombatForms/Form1.Designer.cs
+++ CombatForms/Form1.Designer.cs
@@ -35,11 +35,19 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(34, 64);
+            this.button1.Location = new System.Drawing.Point(123, 284);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -49,7 +57,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(141, 140);
+            this.button2.Location = new System.Drawing.Point(251, 319);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -59,7 +67,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(250, 64);
+            this.button3.Location = new System.Drawing.Point(381, 284);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -70,7 +78,7 @@
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(12, 169);
+            this.richTextBox1.Location = new System.Drawing.Point(123, 348);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(333, 114);
             this.richTextBox1.TabIndex = 3;
@@ -79,7 +87,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 308);
+            this.button4.Location = new System.Drawing.Point(12, 474);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
@@ -89,7 +97,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(270, 308);
+            this.button5.Location = new System.Drawing.Point(487, 474);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 5;
@@ -99,7 +107,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(141, 91);
+            this.button6.Location = new System.Drawing.Point(251, 284);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 6;
@@ -107,11 +115,95 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.Flee_Click);
             // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(96, 20);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 113);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(186, 20);
+            this.richTextBox3.TabIndex = 8;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(466, 27);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(96, 20);
+            this.richTextBox4.TabIndex = 9;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(381, 113);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(181, 20);
+            this.richTextBox5.TabIndex = 10;
+            this.richTextBox5.Text = "";
+            this.richTextBox5.TextChanged += new System.EventHandler(this.richTextBox5_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 53);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(186, 23);
+            this.progressBar1.TabIndex = 11;
+            this.progressBar1.Value = 100;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(8, 139);
+            this.progressBar2.Maximum = 50;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(190, 23);
+            this.progressBar2.TabIndex = 12;
+            this.progressBar2.Value = 50;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(381, 139);
+            this.progressBar3.Maximum = 50;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.progressBar3.Size = new System.Drawing.Size(181, 23);
+            this.progressBar3.TabIndex = 13;
+            this.progressBar3.Value = 50;
+            this.progressBar3.Click += new System.EventHandler(this.progressBar3_Click);
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(381, 53);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.progressBar4.Size = new System.Drawing.Size(185, 23);
+            this.progressBar4.TabIndex = 14;
+            this.progressBar4.Value = 100;
+            this.progressBar4.Click += new System.EventHandler(this.progressBar4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(357, 343);
+            this.ClientSize = new System.Drawing.Size(574, 509);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -135,6 +227,14 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
     }
 }
 
--- CombatForms/Form1.cs
+++ CombatForms/Form1.cs
@@ -17,6 +17,7 @@ namespace CombatForms
         //bool defend = false;
         //Random flee = new Random();
         Combat active = new Combat();
+        bool HitCheck = false;
         public delegate void Callback();
         FiniteStateMachine<GameStart> FSM;
         public enum GameStart
@@ -32,7 +33,7 @@ namespace CombatForms
         Entity cl = new Entity(100, "Cloud", true);
         Entity ae = new Entity(50, "Aeris the Archer", true);
         Entity ds = new Entity(100, "Dwarf Soilder", true);
-        Entity da = new Entity(50, "Dwark Archer", true);
+        Entity da = new Entity(50, "Dwarf Archer", true);
 
         public void AliveCheck()
         {
@@ -76,8 +77,15 @@ namespace CombatForms
             FSM.AddTransition(GameStart.ENDTURN, GameStart.FLEE);
 
             FSM.Start(GameStart.INIT);
-            richTextBox1.Text += "Current State:" + FSM.GetState().Name;
+            richTextBox1.Text += "Current State: " + FSM.GetState().Name;
             richTextBox1.Text += "\nActive Entity: " + active.activeParty.activePlaya.Name;
+
+            richTextBox2.Text = cl.Name;
+            progressBar1.Value = (int)cl.Health;
+
+            richTextBox3.Text = ae.Name;
+            richTextBox4.Text = ds.Name;
+            richTextBox5.Text = da.Name;
         }
         private void Form1_Load(object sender, EventArgs e) { }
         private void richTextBox1_TextChanged(object sender, EventArgs e) { }
@@ -87,60 +95,71 @@ namespace CombatForms
 
             richTextBox1.Text = active.activeParty.activePlaya.Name + " Chose to: " + FSM.GetState().Name + "\n";
 
-            if (cl.alive == true && active.activeParty.activePlaya.Name == "Cloud")
-                cl.DoDamage(ds);
-            else if (cl.alive == true && active.activeParty.activePlaya.Name == "Cloud" && ds.Health <= 0)
-                cl.DoDamage(ds);
-            else if (ae.alive == true && active.activeParty.activePlaya.Name == "Aeris the Archer")
+            while (HitCheck == false)
             {
-                ae.DoDamage(da);
-                ae.DoDamage(ds);
+                if (cl.alive == true && active.activeParty.activePlaya.Name == "Cloud")
+                {
+                    cl.DoDamage(ds);
+                    HitCheck = true;
+                    progressBar4.Value = (int)ds.Health;
+                }
+                else if (cl.alive == true && active.activeParty.activePlaya.Name == "Cloud" && ds.Health <= 0)
+                {
+                    cl.DoDamage(da);
+                    progressBar3.Value = (int)da.Health;
+                    HitCheck = true;
+                }
+                else if (ae.alive == true && active.activeParty.activePlaya.Name == "Aeris the Archer")
+                {
+                    ae.DoDamage(da);
+                    ae.DoDamage(ds);
+                    progressBar4.Value = (int)ds.Health;
+                    progressBar3.Value = (int)da.Health;
+                    HitCheck = true;
+                }
+                else if (ds.alive == true && active.activeParty.activePlaya.Name == "Dwarf Soilder")
+                {
+                    ds.DoDamage(cl);
+                    progressBar1.Value = (int)cl.Health;
+                    HitCheck = true;
+                }
+                else if (ds.alive == true && active.activeParty.activePlaya.Name == "Dwarf Soilder" && cl.Health <= 0)
+                {
+                    ds.DoDamage(ae);
+                    progressBar2.Value = (int)ae.Health;
+                    HitCheck = true;
+                }
+                else if (da.alive == true && active.activeParty.activePlaya.Name == "Dwarf Archer")
+                {
+                    da.DoDamage(ae);
+                    da.DoDamage(cl);
+                    progressBar2.Value = (int)ae.Health;
+                    progressBar2.Value = (int)cl.Health;
+                    HitCheck = true;
+                }
             }
-            else if (ds.alive == true && active.activeParty.activePlaya.Name == "Dwarf Soilder")
-                ds.DoDamage(cl);
-            else if (ds.alive == true && active.activeParty.activePlaya.Name == "Dwarf Soilder" && cl.Health <= 0)
-                ds.DoDamage(ae);
-            else if (da.alive == true && active.activeParty.activePlaya.Name == "Dwarf Archer")
-            {
-                da.DoDamage(ae);
-                da.DoDamage(cl);
-            }
-
             AliveCheck();
-
-            //Player.DoDamage(Enemy);
-            //richTextBox1.Text += "\nYou attacked, Enemy's Hp: " + Enemy.Health;
         }
         private void EndTurn_Click(object sender, EventArgs e)
         {
             FSM.ChangeState(GameStart.ENDTURN);
             active.activeParty.activePlaya.EndTurn();
-            richTextBox1.Text = active.activeParty.activePlaya.Name + " Chose to:" + FSM.GetState().Name;
+            richTextBox1.Text = active.activeParty.activePlaya.Name + " Chose to: " + FSM.GetState().Name;
             richTextBox1.Text += "\nActive Entity: " + active.activeParty.activePlaya.Name;
-
-            //if (defend == false)
-            //{
-            //    Enemy.DoDamage(Player);
-            //    richTextBox1.Text += "\nEnemy attacked you! Your Hp: " + Player.Health;
-            //}
-            //else
-            //    richTextBox1.Text += "\nEnemy attacked you! Blocked! Your Hp: " + Player.Health;
+            HitCheck = false;
         }
         private void Defend_Click(object sender, EventArgs e)
         {
             FSM.ChangeState(GameStart.DEFEND);
-            richTextBox1.Text = active.activeParty.activePlaya.Name + " Chose to:" + FSM.GetState().Name;
-
-            //defend = true;
+            //richTextBox1.Text = active.activeParty.activePlaya.Name + " Chose to:" + FSM.GetState().Name;
         }
         private void Flee_Click(object sender, EventArgs e)
         {
             FSM.ChangeState(GameStart.FLEE);
-            richTextBox1.Text = "You Chose to:" + FSM.GetState().Name;
-            active.activeParty.activePlaya.alive = false;
-            MessageBox.Show(active.activeParty.activePlaya.Name + " has ran away!");
-            //Enemy.DoDamage(Player);
-            //richTextBox1.Text += "\nYou took extra damage for being a wimp! \nEnemy attacked you! Your Hp: " + Player.Health;
+            //richTextBox1.Text = "You Chose to: " + FSM.GetState().Name;
+            //active.activeParty.activePlaya.TakeDamage(100);
+            //active.activeParty.activePlaya.alive = false;
+            //MessageBox.Show(active.activeParty.activePlaya.Name + " has ran away!");
         }
         private void Save_Click(object sender, EventArgs e)
         {
@@ -151,6 +170,46 @@ namespace CombatForms
         {
             //FSM = DataManager<FiniteStateMachine<GameStart>>.Deserialize("Test");
             //this.richTextBox1.Text = "Current State:" + FSM.GetState().Name;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void progressBar3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox5_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
